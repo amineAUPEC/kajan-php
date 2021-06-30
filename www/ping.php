8e0cad82aaba70f4c89a505c113f09c52a587a51
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-06-30 20:22:22 +0200

    2022 ping php

---
+++ www/ping.php
@@ -0,0 +1,29 @@
+<html>
+<head> <title> ping</title> </head>
+<body>
+<form enctype="multipart/form-data" method="get">
+    <fieldset>
+            <p>
+                <label>Saisir une IP :</label>
+                <input name="ip" type="text" id="ip"/>
+                <input type="submit" name="submit" value="soumettre" />
+            </p>
+   </fieldset>
+</form>
+
+</body>
+</html>
+<?php
+
+
+echo '<pre>';
+echo "L IP est : ";
+echo $_GET['ip'];
+$ip_value=$_GET['ip'];
+
+echo "<br>";
+exec("ping -c 4 -w 2 " . $_GET['ip'], $output);
+print_r($output);
+
+
+?>
