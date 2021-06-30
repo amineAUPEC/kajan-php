030bc7844c24f0e1109a84e0eaae001de3b197dc
amineAUPEC <79785242+amineAUPEC@users.noreply.github.com>
2021-06-30 20:02:28 +0200

    exec

--- www/index.php
+++ www/index.php
@@ -20,17 +20,24 @@
 echo '<pre>';
 echo "L IP est : ";
 echo $_GET['ip'];
-$ip_value=isset($_GET['ip']);
 $ip_value=$_GET['ip'];
+
+
+exec("ping -c 4 " . $_GET['ip'], $output);
+echo "&ltpre>";
+print_r($output);
+echo "&lt/pre>";
+
+
  // Affiche le résultat de la commande "ls" et retourne
  // la dernière lignes dans $last_line. Stocke la valeur retournée
  // par la commande shelle dans $retval.
- $last_line = system("ls $ip_value", $retval);
-//  $last_line = system('ping 192.168.1.23 c 4 -w 2', $retval);
-//  $last_line = system('ls', $retval);
- // Affichage d'autres informations
- echo '
-</pre>
-<hr />La dernière ligne en sortie de la commande : ' . $last_line . '
-<hr />Valeur retournée : ' . $retval;
+//  $last_line = system("ls $ip_value", $retval);
+// //  $last_line = system('ping 192.168.1.23 c 4 -w 2', $retval);
+// //  $last_line = system('ls', $retval);
+//  // Affichage d'autres informations
+//  echo '
+// </pre>
+// <hr />La dernière ligne en sortie de la commande : ' . $last_line . '
+// <hr />Valeur retournée : ' . $retval;
 ?>
